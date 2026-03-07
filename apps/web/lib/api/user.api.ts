d52f38f46bbc4bec6e5685e8864d1b72f32db728
Adil Rafiq <muh.adil.rafiq@gmail.com>
2026-03-08 02:53:43 +0500

    fix userApi

--- apps/web/lib/api/user.api.ts
+++ apps/web/lib/api/user.api.ts
@@ -2,7 +2,7 @@ import { apiClient } from '@/lib/api/client';
 import { UpdateUserProfileInput, UserProfile, UserWithProfile } from '@repo/shared';
 
 export const userApi = {
-  get: (): Promise<UserWithProfile> => apiClient.get('/api/users/me').json(),
+  get: (): Promise<UserWithProfile> => apiClient.get('api/users/me').json(),
   updateProfile: (input: UpdateUserProfileInput): Promise<UserProfile> =>
-    apiClient.patch('/api/users/me/profile', { json: input }).json(),
+    apiClient.put('api/users/me/profile', { json: input }).json(),
 };
