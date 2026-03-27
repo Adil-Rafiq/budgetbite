bc0fce6eb27803d6599ccbdd97af54282ff19aa6
Adil Rafiq <muh.adil.rafiq@gmail.com>
2026-03-27 23:28:06 +0500

    fix admin api structure

--- apps/api/src/routes/admin.routes.ts
+++ apps/api/src/routes/admin.routes.ts
@@ -12,10 +12,10 @@ router.get('/restaurants', adminController.listRestaurants);
 router.get('/restaurants:id', adminController.getRestaurantById);
 // GET /restaurants/external/:externalId
 router.get('/restaurants/external/:externalId', adminController.getRestaurantByExternalId);
-
 router.post('/restaurants', adminController.createRestaurant);
 router.patch('/restaurants/:id', adminController.updateRestaurant);
 router.delete('/restaurants/:id', adminController.deleteRestaurant);
+
 router.post('/restaurants/:id/menu-items', adminController.createMenuItems);
 router.patch('/restaurants/:id/menu-items/:itemId', adminController.updateMenuItem);
 router.delete('/restaurants/:id/menu-items/:itemId', adminController.deleteMenuItem);
