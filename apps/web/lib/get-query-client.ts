fb1a7a340282fee90e36e4c06dfc4d5ab6c0a35a
Adil Rafiq <muh.adil.rafiq@gmail.com>
2026-04-18 17:37:42 +0500

    add global retry checks for query client

--- apps/web/lib/get-query-client.ts
+++ apps/web/lib/get-query-client.ts
@@ -1,11 +1,17 @@
 // this code from tanstack official docs
 import { QueryClient, defaultShouldDehydrateQuery, isServer } from '@tanstack/react-query';
+import { isNotFound, isUnauthorized } from '@/lib/api/errors';
 
 function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 60 * 1000,
+        retry: (failureCount, err) => {
+          if (isNotFound(err)) return false;
+          if (isUnauthorized(err)) return false;
+          return failureCount < 3;
+        },
+        staleTime: 1000 * 60, // 1 minute
       },
       dehydrate: {
         // include pending queries in dehydration
